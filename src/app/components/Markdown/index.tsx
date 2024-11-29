b076209d2451b3c4e00d7d2cadaec66b52e8ff86
Justin <spotty118@gmail.com>
2024-11-28 21:08:18 -0600

    Fix TypeScript errors in CopyToClipboard components and rebuild

--- src/app/components/Markdown/index.tsx
+++ src/app/components/Markdown/index.tsx
@@ -32,17 +32,19 @@ function CustomCode({ children, className }: CustomCodeProps) {
     <div className="flex flex-col">
       <div className="bg-[#e6e7e8] dark:bg-[#444a5354] text-xs p-2">
         <CopyToClipboard text={code} onCopy={() => setCopied(true)}>
-          {copied ? (
-            <div className="flex flex-row items-center gap-2 w-fit ml-1 cursor-pointer">
-              <BsClipboard />
-              <span>copied</span>
-            </div>
-          ) : (
-            <div className="flex flex-row items-center gap-2 w-fit ml-1 cursor-pointer">
-              <BsClipboard />
-              <span>copy code</span>
-            </div>
-          )}
+          <div>
+            {copied ? (
+              <div className="flex flex-row items-center gap-2 w-fit ml-1 cursor-pointer">
+                <BsClipboard />
+                <span>copied</span>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center gap-2 w-fit ml-1 cursor-pointer">
+                <BsClipboard />
+                <span>copy code</span>
+              </div>
+            )}
+          </div>
         </CopyToClipboard>
       </div>
       <code className={cx(className, 'px-4')}>{children}</code>
